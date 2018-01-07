c92f7e9aab53901fc8f190a9aaaef0cf7f8fac0e
Horie Kazuaki <hazemi94basic@gmail.com>
2018-01-08 00:59:41 +0900

    top画面のhtml部分を作成
    top画面のcssを用いないhtml部分のみのコーディングを行った
    srcフォルダを作成。この中にcssやjs, 画像データを置くこととする

---
+++ index.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>はてな匿名ダイアリー</title>
+  </head>
+  <body>
+    <header>
+      <img src="./src/image/common/hatelabo.png" alt="Hatelabo::">
+      <img src="./src/image/common/anonymous_diary.png" alt="AnonymousDiary">
+    </header>
+    <div id="user">
+      <div id="user-name">
+        ようこそhogeさん
+      </div>
+      <ul id="user-setting">
+        <li>hogeの日記</li>
+        <li>日記を書く</li>
+        <li>ログアウト</li>
+      </ui>
+    </div>
+    <!-- パンくずリスト(タイトルではちょっと違うが) -->
+    <div id="breadcrumbs">
+      <ul>
+        <li><前の25件</li>
+        <li>次の25件></li>
+      </ul>
+    </div>
+    <main>
+      <div class="date-contents">
+        <div class="date">
+          2018-01-17
+        </div>
+        <div class="articles">
+          <div class="diary">
+            <div class="link">
+              ■
+            </div>
+            <div class="title">
+              タイトル
+            </div>
+            <div class="text-body">
+                本文はここに入ります
+            </div>
+            <div class="menu">
+              <ul>
+                <li>Permalink</li>|
+                <li>記事への反応(0)</li>|
+                <li>23:17</li>|
+              </ul>
+            </div>
+          </div>
+          <div class="diary">
+            <div class="link">
+              ■
+            </div>
+            <div class="title">
+              anound:20180107012345
+            </div>
+            <div class="text-body">
+                本文はここに入ります
+            </div>
+            <div class="menu">
+              <ul>
+                <li>Permalink</li>|
+                <li>記事への反応(0)</li>|
+                <li>23:17</li>|
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div class="date">
+          2018-01-18
+        </div>
+        <div class="articles">
+          <div class="diary">
+            <div class="link">
+              ■
+            </div>
+            <div class="title">
+              タイトル
+            </div>
+            <div class="text-body">
+                本文はここに入ります
+            </div>
+            <div class="menu">
+              <ul>
+                <li>Permalink</li>|
+                <li>記事への反応(0)</li>|
+                <li>23:17</li>|
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  </body>
+</html>
