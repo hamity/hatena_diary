84ac6a42116a3e4cfd46965bd4bcc9f390443713
hamity <hazemi94basic@gmail.com>
2018-04-12 21:25:54 +0900

    作り直し。URIを取得して配列を返すメソッドを作成

--- Dispatcher.php
+++ Dispatcher.php
@@ -1,67 +1,46 @@
 <?php
 class Dispatcher
 {
-  //アクセス権(publicやprivate)を明示せずに定義した場合はpublicになる
-  /*
-  const CONTROLLER_NAME = array('index',
-  'login',
-  'logout');
-  */
-
   public function dispatch()
   {
-    $uri = $_SERVER['REQUEST_URI'];
-    $uri = '928520';
+    echo 'Dispacher Class Call!!<br>';
+    $uri = '/hatena_diary/hogehoge/foo';    //テスト用
 
-    $uri_normalization = $this->replaceUri($uri);
+    $uri_array = $this->replaceUri($uri);
+    $uri_array = $this->replaceUri($_SERVER['REQUEST_URI']);
 
-    $param = explode('/', $uri_normalization);
-    echo $param[0];
+    // $param = explode('/', $uri_normalization);
 
-    $controller_argument = '';
-    if(strlen($param[0]) === 0 || ctype_digit($param[0]) === true)
-    {
-      $controller_argument = $param[0];
-      echo $controller_argument;
-      $param[0] = 'index';
-    }
+    // $controller_argument = '';
+    // if(strlen($param[0]) === 0 || ctype_digit($param[0]) === true)
+    // {
+    //   $controller_argument = $param[0];
+    //   $param[0] = 'index';
+    // }
 
-    //ユーザー名の場合の処理
-    /*
-    for($i = 0; $i < count($this->CONTROLLER_NAME); $i++)
-    {
-      if($CONTROLLER_NAME[$i] === $param[0])
-      {
-        break;
-      }
-      $controller_argument = $param[0];
-      $param[0] = 'user';
-    }
-    */
+    // $class_name = ucfirst(strtolower($param[0])).'Controller';
+    // require_once(dirname(__FILE__).'/controllers/'.$class_name.'.php');
+    // echo $class_name.'<br>';
+    // $controller = new $class_name($controller_argument);
 
-    $class_name = ucfirst(strtolower($param[0])).'Controller';
-    require_once(dirname(__FILE__).'/controllers/'.$class_name.'.php');
-    echo $class_name.'<br>';
-    $controller = new $class_name($controller_argument);
-
-    //URIに呼び出すメソッド名が書かれていない場合はindexAction()を呼び出す
-    $get_name = 'index';
-    $action_argument = '';
-    if(count($param) > 1)
-    {
-      $get_name = $param[1];
-    }
-    $get_method_name = $get_name.'Action';
-    echo $get_method_name.'<br>';
-    $controller->$get_method_name($action_argument);
+    // //URIに呼び出すメソッド名が書かれていない場合はindexAction()を呼び出す
+    // $get_name = 'index';
+    // $action_argument = '';
+    // if(count($param) > 1)
+    // {
+    //   $get_name = $param[1];
+    // }
+    // $get_method_name = $get_name.'Action';
+    // echo $get_method_name.'<br>';
+    // $controller->$get_method_name($action_argument);
   }
 
-  function replaceUri($uri)
+  // /hatena_diary以下のURIの配列化. /hatena_diary/hoge/fooの場合、['hoge', 'foo']を返す
+  function replaceUri($url)
   {
-        // /hatena_diary/だと空白が代入される
-        $uri_normalization = ereg_replace('^(/hatena_diary/)','', $uri);
-        $uri_normalization = ereg_replace('\.([a-z]+)$', '', $uri_normalization);
-        $uri_normalization = ereg_replace('/$', '', $uri_normalization);
-        return $uri_normalization;
+    $pass = parse_url($url, PHP_URL_PATH);    // TODO これはいるのか？
+    $remove_hatena_diary = str_replace('/hatena_diary', '', $pass);
+    $url_normalization = ltrim($remove_hatena_diary, '/');
+    return explode('/', $url_normalization);
   }
 }
